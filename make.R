72f5946fb651d9a4d42109ef0f9816488ca2c297
Lisa Nicvert <lisa.nicvert@fondationbiodiversite.fr>
2026-04-16 16:51:12 +0200

    new outputs (20 and 50 km)

--- make.R
+++ make.R
@@ -21,29 +21,29 @@ devtools::load_all()
 library(here)
 
 ## Run Project ----
-# 1 Simplify the grid
-message("1. Simplify the grid -------------------------------")
-source(here("analyses", "01_simplify_grid.R"))
-
-# 2. Get the land cover
-# Corine land cover, 100m, 2018, from
-# https://land.copernicus.eu/en/products/corine-land-cover/clc2018
-# https://doi.org/10.2909/960998c1-1870-4e82-8051-6485205ebbac
-message("2. Get the land cover -------------------------------")
-source(here("analyses", "02_get_landcover.R"))
-
-# 3. Get the bioclimatic variables
-# Chelsa 2, 1km, average 1981-2010, from
-# Karger, D.N., Conrad, O., Böhner, J., Kawohl, T., Kreft, H., Soria-Auza, R.W., Zimmermann, N.E., Linder, P., Kessler, M. (2017).
-# Climatologies at high resolution for the Earth land surface areas. Scientific Data. 4 170122. https://doi.org/10.1038/sdata.2017.122
-# https://chelsa-climate.org
-message("3. Get the bioclimatic variables -------------------------------")
-source(here("analyses", "03_get_bioclim.R"))
-
-# 4. Get mean yearly temperatures
-# All data downloaded in script here: https://www.chelsa-climate.org/datasets/chelsa_monthly
-# message("4. Get mean yearly temperatures -------------------------------")
-source(here("analyses", "04_get_tempyear.R"))
+# # 1 Simplify the grid
+# message("1. Simplify the grid -------------------------------")
+# source(here("analyses", "01_simplify_grid.R"))
+# 
+# # 2. Get the land cover
+# # Corine land cover, 100m, 2018, from
+# # https://land.copernicus.eu/en/products/corine-land-cover/clc2018
+# # https://doi.org/10.2909/960998c1-1870-4e82-8051-6485205ebbac
+# message("2. Get the land cover -------------------------------")
+# source(here("analyses", "02_get_landcover.R"))
+# 
+# # 3. Get the bioclimatic variables
+# # Chelsa 2, 1km, average 1981-2010, from
+# # Karger, D.N., Conrad, O., Böhner, J., Kawohl, T., Kreft, H., Soria-Auza, R.W., Zimmermann, N.E., Linder, P., Kessler, M. (2017).
+# # Climatologies at high resolution for the Earth land surface areas. Scientific Data. 4 170122. https://doi.org/10.1038/sdata.2017.122
+# # https://chelsa-climate.org
+# message("3. Get the bioclimatic variables -------------------------------")
+# source(here("analyses", "03_get_bioclim.R"))
+# 
+# # 4. Get mean yearly temperatures
+# # All data downloaded in script here: https://www.chelsa-climate.org/datasets/chelsa_monthly
+# # message("4. Get mean yearly temperatures -------------------------------")
+# source(here("analyses", "04_get_tempyear.R"))
 
 # 5. Format environmental variables
 message("5. Format environmental variables -------------------------------")
